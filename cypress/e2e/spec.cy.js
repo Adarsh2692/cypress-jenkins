70b50a0fb7f897b3c6a6201f30ae6e7e67c9f85e
Adarsh Singh <80701878+Adarsh2692@users.noreply.github.com>
2023-01-31 15:41:53 +0530

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,30 +8,4 @@ describe('Verify Browser Stack Home Page', () => {
 
   })
 
-  it('Verify Header is present', () => {
-
-      cy.get('h1').first().should('be.exist');
-
-  })
-
-  it('Verify Product menu are present', () => {
-
-      cy.get('#product-menu-toggle').first().should('be.exist');
-
-      cy.get('#developers-menu-toggle').first().should('be.exist');
-
-      cy.get('#developers-menu-toggle').first().should('be.exist');
-
-  })
-
-  it('Verify menu Links are  present', () => {
-
-      cy.get("a[title='Pricing']").first().should('be.exist');
-
-      cy.get("a[title='Sign In']").first().should('be.exist');
-
-      cy.get("a[title='Live for Teams']").first().should('be.exist');
-
-  })
-
 })
